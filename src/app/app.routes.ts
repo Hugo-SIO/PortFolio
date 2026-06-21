0a6d21faaa56f2bb9cc725e13622aecc46869721
Frifri26082005 <halmazanlaforet@gmail.com>
2026-06-21 20:31:42 +0200

    commit initial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
+  { path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.About) },
+  { path: 'projects', loadComponent: () => import('./pages/projects/projects').then(m => m.Projects) },
+  { path: 'projects/:id', loadComponent: () => import('./pages/project-detail/project-detail').then(m => m.ProjectDetail) },
+  { path: 'skills', loadComponent: () => import('./pages/skills/skills').then(m => m.Skills) },
+  { path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.Contact) },
+  { path: '**', redirectTo: '' }
+];
