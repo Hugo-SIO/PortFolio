25bb2d417e075c23ee25cbbe655a2f0444b0d3bd
Frifri26082005 <halmazanlaforet@gmail.com>
2026-06-21 21:04:44 +0200

    init commit  projetPage

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+interface Project { id: number; title: string; description: string; tags: string[]; }
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './projects.html',
-  styleUrl: './projects.scss',
+  styleUrl: './projects.scss'
 })
 export class Projects {
-
+  projects: Project[] = [
+    // { id: 1, title: '', description: '', tags: [] }
+  ];
 }
