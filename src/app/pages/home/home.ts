93026c24cc579c2692250aa1894bd81212d9dc1c
Frifri26082005 <halmazanlaforet@gmail.com>
2026-06-21 20:51:38 +0200

    commit init HomePage

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrl: './home.scss'
 })
-export class Home {
-
-}
+export class Home {}
