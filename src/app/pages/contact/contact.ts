483c0e4f2225138f7f8d92d5b7205fb53f8ecb74
Frifri26082005 <halmazanlaforet@gmail.com>
2026-06-22 18:31:42 +0200

    mise à jour page contact

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, LucideAngularModule],
+  imports: [FormsModule, HttpClientModule, LucideAngularModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
-  onSubmit() {}
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit(form: any) {
+
+    const data = {
+      name: form.value.name,
+      email: form.value.email,
+      subject: form.value.subject, // ✅ ajouté
+      message: form.value.message
+    };
+
+    this.http.post('http://localhost:3000/contact', data)
+      .subscribe({
+        next: () => {
+          alert("Message envoyé !");
+          form.reset();
+        },
+        error: () => alert("Erreur envoi")
+      });
+  }
 }
